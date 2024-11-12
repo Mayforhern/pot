54cb232b549d47642681db83c389040096cc18b0
mayforhern <mayankdindoire@gmail.com>
2024-11-13 02:22:45 +0530

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 // next.config.js (root of your project)
 module.exports = {
-    reactStrictMode: true, // Enable React Strict Mode for better development feedback
+    reactStrictMode: true,  // Enable React Strict Mode for better development feedback
+    images: {
+      domains: ['i.postimg.cc'], // Add this line
+    },
   };
   
